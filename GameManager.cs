35c61e22c7fbf8737fe192fdd0452909d0d4d543
shototype <mattnewmanfrfr@gmail.com>
2024-11-16 23:31:41 -0500

    Update GameManager.cs
    Includes all tasks up till now, changed coin and h-up spawn locations to fit with movement limitations

--- GameManager.cs
+++ GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameManager : MonoBehaviour
@@ -10,11 +11,24 @@ public class GameManager : MonoBehaviour
     public GameObject player;
     public GameObject enemyOne;
     public GameObject cloud;
-    public GameObject HealthPUp;
-    public TextMeshProUGUI scoreText;
+    public GameObject powerup;
     public GameObject enemyTwo;
     public GameObject enemyThree;
     public GameObject Coin;
+    public GameObject HealthPUp;
+
+
+    public AudioClip powerUp;
+    public AudioClip powerDown;
+
+    public int cloudSpeed;
+
+    private bool isPlayerAlive;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI restartText;
+    public TextMeshProUGUI powerupText;
     public TextMeshProUGUI livesText;
 
     private int score;
@@ -24,6 +38,7 @@ public class GameManager : MonoBehaviour
     {
         Instantiate(player, transform.position, Quaternion.identity);
         InvokeRepeating("CreateEnemyOne", 1f, 3f);
+        StartCoroutine(CreatePowerup());
         CreateSky();
         score = 0;
         scoreText.text = "Score: " + score;
@@ -32,12 +47,14 @@ public class GameManager : MonoBehaviour
         InvokeRepeating("CreateEnemyThree", 2f, 7f);
         livesText.text = "Lives: 3";
         InvokeRepeating("CreateCoin", 2f, 6f);
+        isPlayerAlive = true;
+        cloudSpeed = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Restart();   
     }
 
     void CreateEnemyOne()
@@ -53,6 +70,13 @@ public class GameManager : MonoBehaviour
     void CreateEnemyThree()
     {
         Instantiate(enemyThree, new Vector3(11f, Random.Range(-2f, 9f), 0), Quaternion.Euler(0, 0, 115));
+    }
+
+    IEnumerator CreatePowerup()
+    {
+        Instantiate(powerup, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.identity);
+        yield return new WaitForSeconds(Random.Range(3f, 6f));
+        StartCoroutine(CreatePowerup());
     }
 
     void CreateSky()
@@ -71,17 +95,48 @@ public class GameManager : MonoBehaviour
 
     void CreateHPUp()
     {
-        Instantiate(HealthPUp, new Vector3(Random.Range(-8f, 8f), Random.Range(-8f, 8f), 0), Quaternion.identity);
+        Instantiate(HealthPUp, new Vector3(Random.Range(-8f, 8f), Random.Range(-5f, 0f), 0), Quaternion.identity);
     }
 
     void CreateCoin()
     {
-        Instantiate(Coin, new Vector3(Random.Range(-8f, 8f), Random.Range(-8f, 8f), 0), Quaternion.identity);
+        Instantiate(Coin, new Vector3(Random.Range(-8f, 8f), Random.Range(-5f, 0f), 0), Quaternion.identity);
     }
-    
+
     public void LoseLifeText(int currentLives)
     {
         livesText.text = "Lives: " + currentLives;
     }
 
+    public void GameOver()
+    {
+        isPlayerAlive = false;
+        CancelInvoke();
+        gameOverText.gameObject.SetActive(true);
+        restartText.gameObject.SetActive(true);
+        cloudSpeed = 0;
+    }
+
+    void Restart()
+    {
+        if(Input.GetKeyDown(KeyCode.R) && isPlayerAlive == false)
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+
+    public void UpdatePowerupText(string whichPowerup)
+    {
+        powerupText.text = whichPowerup;
+    }
+
+    public void PlayPowerUp()
+    {
+        AudioSource.PlayClipAtPoint(powerUp, Camera.main.transform.position);
+    }
+
+    public void PlayPowerDown()
+    {
+        AudioSource.PlayClipAtPoint(powerDown, Camera.main.transform.position);
+    }
 }
